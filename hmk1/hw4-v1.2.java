88fae48416825d5b08cd3202002dfd2125af40a5
Abdelrahman Elmughrabi <abdrahman.mughrabi14@gmail.com>
2024-01-10 16:34:55 +0300

    satisfying requirement three

--- hmk1/hw4-v1.2.java
+++ hmk1/hw4-v1.2.java
@@ -38,6 +38,7 @@ public class hw4 {
 
         System.out.println("Numbers between " + start + " and " + end + ":");
         for (int i = start + 1; i < end; i++) {
+            if(i%2==0)
             System.out.print(i + " ");
         }
     }
